ea6311f2cd13f4ecd178fc328bfc8714ba2e34d2
moster-source <gokovac@gmail.com>
2022-06-24 10:05:59 +0200

    kontrolna tocka
    Sve radi

--- Brojac/Form1.Designer.cs
+++ Brojac/Form1.Designer.cs
@@ -38,10 +38,10 @@
             // lblExit
             // 
             this.lblExit.AutoSize = true;
-            this.lblExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.lblExit.Location = new System.Drawing.Point(92, -1);
+            this.lblExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblExit.Location = new System.Drawing.Point(115, 0);
             this.lblExit.Name = "lblExit";
-            this.lblExit.Size = new System.Drawing.Size(15, 16);
+            this.lblExit.Size = new System.Drawing.Size(20, 20);
             this.lblExit.TabIndex = 0;
             this.lblExit.Text = "X";
             this.lblExit.Click += new System.EventHandler(this.lblExit_Click);
@@ -49,58 +49,64 @@
             // lblVrijeme
             // 
             this.lblVrijeme.AutoSize = true;
-            this.lblVrijeme.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblVrijeme.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.lblVrijeme.Location = new System.Drawing.Point(0, -1);
+            this.lblVrijeme.Margin = new System.Windows.Forms.Padding(0);
             this.lblVrijeme.Name = "lblVrijeme";
-            this.lblVrijeme.Size = new System.Drawing.Size(86, 32);
+            this.lblVrijeme.Size = new System.Drawing.Size(112, 42);
             this.lblVrijeme.TabIndex = 1;
             this.lblVrijeme.Text = "00:00";
             this.lblVrijeme.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lblVrijemeMouseDown);
+            this.lblVrijeme.MouseUp += new System.Windows.Forms.MouseEventHandler(this.lblVrijemeMouseUp);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(92, 15);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(115, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(16, 16);
+            this.label1.Size = new System.Drawing.Size(20, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "S";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // txtVrijeme
             // 
-            this.txtVrijeme.Location = new System.Drawing.Point(6, 46);
+            this.txtVrijeme.Location = new System.Drawing.Point(7, 12);
             this.txtVrijeme.Name = "txtVrijeme";
             this.txtVrijeme.Size = new System.Drawing.Size(35, 22);
-            this.txtVrijeme.TabIndex = 3;
+            this.txtVrijeme.TabIndex = 1;
+            this.txtVrijeme.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtVrijemeEnter);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(47, 52);
+            this.label2.Location = new System.Drawing.Point(48, 18);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 16);
+            this.label2.Size = new System.Drawing.Size(84, 16);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Spremi";
+            this.label2.Text = "Unesi minute";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(113, 80);
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(138, 45);
             this.ControlBox = false;
+            this.Controls.Add(this.lblExit);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtVrijeme);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblVrijeme);
-            this.Controls.Add(this.lblExit);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Brojac/Form1.cs
+++ Brojac/Form1.cs
@@ -17,7 +17,6 @@ namespace Brojac
 
 {
 
-
     public partial class Form1 : Form
     {
         public const int WM_NCLBUTTONDOWN = 0xA1;
@@ -27,8 +26,6 @@ namespace Brojac
         public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
         [DllImportAttribute("user32.dll")]
         public static extern bool ReleaseCapture();
-
-
         public static System.Windows.Forms.Timer t = new System.Windows.Forms.Timer();
         public static int min;
         public static int sec;
@@ -46,7 +43,8 @@ namespace Brojac
             min = 15;
             minEntered = 15;
             sec = 0;
-
+            txtVrijeme.Visible = false;
+            label2.Visible = false;
         }
 
         private void lblExit_Click(object sender, EventArgs e)
@@ -68,14 +66,26 @@ namespace Brojac
             if (t.Enabled)
             {
                 t.Stop();
-                sec=0;
+                sec = 0;
                 min = minEntered;
                 return;
             }
-            else 
+            else
             {
-                //if backcolor red then poplavi
-                t.Start();
+                if (this.BackColor == Color.Red)
+                {
+                    pocrveni(false);
+                }
+
+                if (min == 0)
+                {
+                    return;
+                }
+                sec = 59;
+                min = minEntered - 1;
+                //this.lblVrijeme.Text = min.ToString() + ":" + sec.ToString();
+                this.lblVrijeme.Text = String.Format("{0:00}", min) + ":" + String.Format("{0:00}", sec);
+                t.Enabled = true;
             }
         }
 
@@ -89,38 +99,99 @@ namespace Brojac
                     //lblVrijeme.Text = "";
                 }
                 if (min == 0) {
-                    lblVrijeme.Text =min.ToString() + ":" + sec.ToString();
                     min = minEntered;
+                    SystemSounds.Beep.Play();
+                    //lblVrijeme.Text = minEntered.ToString() + ":" + sec.ToString();
+                    this.lblVrijeme.Text = String.Format("{0:00}", min) + ":" + String.Format("{0:00}", sec);
+                    pocrveni(true);
                     t.Stop();
                     return;
                 }
             }
-            
-            lblVrijeme.Text = min.ToString() + ":" + sec.ToString();
+            //lblVrijeme.Text = min.ToString() + ":" + sec.ToString();
+            this.lblVrijeme.Text = String.Format("{0:00}", min) + ":" + String.Format("{0:00}", sec);
             sec = sec - 1;
             
             if (sec == -1) {
                 sec = 59;
                 min = min - 1;
             }
-
         }
         private void label2_Click(object sender, EventArgs e)
         {
             try
             {
                 min = Convert.ToInt32(txtVrijeme.Text);
-                lblVrijeme.Text = min.ToString() + ":" + "00";
+                minEntered = min;
                 sec = 0;
+                //lblVrijeme.Text = min.ToString() + ":" + "00";
+                this.lblVrijeme.Text = String.Format("{0:00}", min) + ":" + String.Format("{0:00}", sec);
+                txtVrijeme.Visible = false;
+                txtVrijeme.Text = "";
+                label2.Visible = false;
+                lblVrijeme.Visible = true;
+                label1.Visible = true;
+                
             }
             catch (FormatException)
             {
                 min = 0;
                 min = 0;
-                lblVrijeme.Text = min.ToString() + ":" + "00";
+                //lblVrijeme.Text = min.ToString() + ":" + "00";
+                this.lblVrijeme.Text = String.Format("{0:00}", min) + ":" + String.Format("{0:00}", sec);
+                txtVrijeme.Visible = false;
+                txtVrijeme.Text = "";
+                label2.Visible = false;
+                lblVrijeme.Visible = true;
+                label1.Visible = true;
             }
-            
-
         }
+        private void pocrveni(Boolean blnCrveni) {
+            if (blnCrveni)
+            {
+                //ControlLight
+                this.BackColor = Color.Red;
+                this.lblVrijeme.BackColor = Color.Red;
+            }
+            else
+            {
+                this.BackColor = SystemColors.Control;
+                this.lblVrijeme.BackColor = SystemColors.Control;
+            }
+              
+        }
+
+        private void txtVrijemeEnter(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                this.label2_Click(sender,e);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //min = 0;
+            //sec = 0;
+            this.lblVrijeme.Text = String.Format("{0:00}", min) + ":" + String.Format("{0:00}", sec);
+            //lblVrijeme.Text = string.Format("{0:00}", min);
+        }
+
+        private void lblVrijemeMouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                //ReleaseCapture();
+                txtVrijeme.Visible = true;
+                label2.Visible = true;
+                label1.Visible = false;
+                lblVrijeme.Visible=false;
+                pocrveni(false);
+                this.txtVrijeme.Focus();
+                //MessageBox.Show("", "");
+                t.Stop();
+            }
+        }
+
     }
 }
